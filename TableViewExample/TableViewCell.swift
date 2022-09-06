be742b3346c2bc0ab5d6f3b8e52611e1e2721980
Greed1997 <43263569+Greed1997@users.noreply.github.com>
2022-09-06 12:49:46 +0300

    Initial

---
+++ TableViewExample/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TableViewExample
+//
+//  Created by Александр on 05.09.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
